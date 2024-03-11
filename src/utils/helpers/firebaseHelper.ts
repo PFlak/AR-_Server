b499f0c66f5624cdbbfdef146d44efb7a61b8cbb
Michal <michal513717@gmail.com>
2024-03-11 10:13:22 +0100

    Added firebase helper

---
+++ src/utils/helpers/firebaseHelper.ts
@@ -0,0 +1,18 @@
+import firebase from "firebase-admin";
+
+export class FirebaseHelper {
+    public static converterAssignTypes<T extends {}>() {
+        return {
+            toFirestore(doc: T): FirebaseFirestore.DocumentData {
+                return doc;
+            },
+            fromFirestore(snapshot: FirebaseFirestore.QueryDocumentSnapshot): T {
+                return snapshot.data()! as T;
+            }
+        };
+    };
+
+    public static async getServerTimeStamp() {
+        return await firebase.firestore.FieldValue.serverTimestamp();
+    }
+}
