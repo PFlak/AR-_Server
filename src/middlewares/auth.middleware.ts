ff1a37a02bd0eb9944e93ef09ae62885e71265a7
Dominik Kozieł <domino15973@gmail.com>
2024-03-13 14:47:46 +0100

    firebase auth

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,1 +1,32 @@
-//TODO implement
+import admin from '../firebase/index';
+import { Request, Response, NextFunction } from 'express';
+
+declare global {
+    namespace Express {
+        interface Request {
+            user?: any
+        }
+    }
+}
+
+class AuthMiddleware {
+    async decodeToken(req: Request, res: Response, next: NextFunction) {
+        const token: string | undefined = req.headers.authorization?.split(' ')[1];
+        if (!token) {
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+
+        try {
+			const decodedValue = await admin.auth().verifyIdToken(token);
+			if (decodedValue) {
+				req.user = decodedValue;
+				return next();
+			}
+			return res.status(401).json({ message: 'Unauthorized' });
+		} catch (error) {
+			return res.status(500).json({ message: 'Internal Server Error' });
+		}
+    }
+}
+
+export default new AuthMiddleware();
