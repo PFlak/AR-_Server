7ed14f4d0a00e9a7e3fe8c2a8574b2981e339acf
PFlak <piotr.andrzej.flak@gmail.com>
2024-03-14 12:19:34 +0100

    Separation between model and schema

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,18 +1,5 @@
 import { z } from "zod";
 
-const userRole = z.union([z.literal("USER"), z.literal("ADMIN")], {
-  required_error: "role is required",
-});
-
-const userSchema = z.object({
-  user_id: z.string({ required_error: "user_id is required" }),
-  nick: z.string({ required_error: "nick is required" }).min(4),
-  role: userRole,
-  email: z.string({ required_error: "email is required" }).email(),
-  name: z.string().optional(),
-  surname: z.string().optional(),
-  profil_image: z.string().optional(),
-  creation_time: z.date().optional(),
-});
+const userSchema = require("../utils/schemas/user.schema");
 
 export type User = z.infer<typeof userSchema>;
---
+++ src/utils/schemas/user.schema.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const userRole = z.union([z.literal("USER"), z.literal("ADMIN")], {
+  required_error: "role is required",
+});
+
+export const userSchema = z.object({
+  user_id: z.string({ required_error: "user_id is required" }),
+  nick: z.string({ required_error: "nick is required" }).min(4),
+  role: userRole,
+  email: z.string({ required_error: "email is required" }).email(),
+  name: z.string().optional(),
+  surname: z.string().optional(),
+  profil_image: z.string().optional(),
+  creation_time: z.date().optional(),
+});
